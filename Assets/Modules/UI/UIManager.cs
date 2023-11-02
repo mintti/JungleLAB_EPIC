482b46fd9ac8519005d3c5607f16d62304db0120
mintti <mintti97@gmail.com>
2023-11-02 13:55:43 +0900

    [Feat] 플레이어, 보스, 신규 스킬 선택창 관련 스크립트 초안 작성
    - 플레이어 (상태, 스킬/캐스팅 정보, 카드)
    - 보스(상태)
    - 신규 스킬 선택창

--- Assets/Modules/UI/UIManager.cs
+++ Assets/Modules/UI/UIManager.cs
@@ -1,22 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
-    #region Singleton
-    public static UIManager I { get; private set; }
-    private void Awake()
-    {
-        if (I == null)
-        {
-            I = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    #endregion
+    [SerializeField] private UIPlayerInfo _uiPlayerInfo;
+    public UIPlayerInfo UIPlayerInfo => _uiPlayerInfo;
+    
+    [SerializeField] private UIBossInfo _uiBossInfo;
+    public UIBossInfo UIBossInfo => _uiBossInfo;
+    
+    [FormerlySerializedAs("_uiSkillSelector")] [SerializeField] private UINewSkillSelector uiNewSkillSelector;
+    public UINewSkillSelector UINewSkillSelector => uiNewSkillSelector;
 }
