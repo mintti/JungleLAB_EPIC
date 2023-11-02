95aacf9d9702c6c8592c037941ce2c102770bbb9
김은서 <a55208806@gmail.com>
2023-11-02 11:10:50 +0900

    BoardManager 수정

--- Assets/Modules/Board/BoardManager.cs
+++ Assets/Modules/Board/BoardManager.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+    private int _playerOnIndex;
     public List<BaseTile> tiles = new();
+    public List<ISummon> summons = new();
 
+    private void OnTurnEnd()
+    {
+        foreach(BaseTile t in tiles)
+        {
+            t.OnTurnEnd();
+        }
 
+        foreach(ISummon s in summons)
+        {
+            s.OnTurnEnd();
+        }
+    }
+
+    private void Init()
+    {
+        
+    }
+
+    private void GetNextTile()
+    {
+
+    }
 }
