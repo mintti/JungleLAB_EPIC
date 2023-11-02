3c131d29d9489176bab20342e31caecff531c764
김은서 <a55208806@gmail.com>
2023-11-02 11:11:24 +0900

    ISummon 및 파이어볼 추가

---
+++ Assets/Modules/Board/Fireball.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour, ISummon
+{
+    private int index;
+
+    public void Connect()
+    {
+        
+    }
+
+    public void OnTurnEnd()
+    {
+        // 보드에서 두칸 이동
+    }
+}
